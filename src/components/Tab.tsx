43e65193c19d30519917ae4b6e8fd1f13c6df4d7
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-12-23 17:12:35 +0200

    change relative to absolute path in Tab component

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -12,7 +12,8 @@ export const Tab: React.FC<Props> = ({ tab }) => {
 
   return (
     <li data-cy="Tab" className={classNames({ 'is-active': tabId === tab.id })}>
-      <Link to={`${tab.id}`}>{tab.title}</Link>
+      <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
     </li>
   );
 };
+
